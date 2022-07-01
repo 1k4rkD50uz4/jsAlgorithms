7200ab5642f3b5014c934e9ea1d951cae4679905
Mark Dsouza <mark.francis.dsouza@gmail.com>
2022-07-01 11:16:19 -0700

    intermediate commit

--- binarySort.js
+++ binarySort.js
@@ -4,15 +4,40 @@
     let s = "The quick brown fox jumped over the lazy dog",
         iter = s[Symbol.iterator](),
         res = iter.next(),
+        i=0,
         c = res.value,
         arr = [],
+        _s,
         prepend = (function () {
             const compare = val => c < val ? -1 : c > val ? 1 : 0;
-            let c = res.value;
+            let _i, c = res.value, _c;
             let f = function () {
                 let _res = compare(res.value);
                 if (_res == -1) {
                     arr.unshift(res.value);
+                    return true;
+                }
+                else if (_res == 1) {
+                    arr.unshift(s[inc(s.lastIndexOf(res.value))]);
+                    _s = s.slice(inc(s.indexOf(res.value)));
+                    _s = _s.slice(i);
+                    //if (!_s) {
+                        
+
+                    //    //c = c.toLowerCase();
+                    //    return false;
+                    //}
+                    //else {
+                    //    //i = inc(_s.indexOf(res.value));
+                        
+                       
+                    //    c = _s[inc(i)];
+                    //    arr.unshift(c);
+                    //    c = String.fromCharCode(parseInt('' + _i, 16)).toLowerCase();
+                    //    arr.unshift(c);
+                    //    arr.push(_s[0]);
+                    //    return true;
+                    //}
                 }
             };
             return f;
@@ -20,64 +45,36 @@
     function sort() { 
         updateVars();
         while (!res.done) {
-            prepend();
-            updateVars();
+            if (prepend()) {
+                updateVars();
+            }
+            else {
+                continue;
+            }            
         }
     }
-    function updateVars() {
+    function updateVars() {  
         res = iter.next();
         c = res.value;
-        if (res.value == " ") {
-            let i = inc(s.lastIndexOf(res.value));
-            arr.unshift.apply(arr, [String.fromCharCode(parseInt('' + i, 16)).toLowerCase(), s[i]]); 
-            i = inc(inc(i));
-            c = arr.pop();
-            arr.push(s[i]);
-            arr.push(c);
-            res = iter.next(); 
-            c = res.value;
-            res = iter.next();
-            i = 0;
-            c += String.fromCharCode(s[i].toLowerCase().charCodeAt(i));
-            c += res.value;
-            res = iter.next();
-            arr.push(res.value);
-            res = iter.next();
-            arr.splice(inc(i), i, res.value);
-            res = iter.next();
-            arr.push(res.value);
-            res = iter.next();
-            res = iter.next();
-            arr.splice(inc(i), i, res.value);
-            res = iter.next();
-            c = c.split('');
-            c.splice(inc(i), i, res.value);
-            res = iter.next();
-            c.unshift(res.value);
-            res = iter.next();
-            i = arr.length - c.length;
-            c.push(res.value);
-            res = iter.next();
-            c.unshift(res.value);
-            arr = arr.concat(c);
-            res = iter.next();
-            i = inc(i);
-            res = iter.next();
-            arr.splice(i, 0, res.value);
-            i++;
-            res = iter.next();
-            i++;
-            c = res.value;
-            res = iter.next();
-            c += res.value;
-            i++;
-            res = iter.next();
-            i++;
-            res = iter.next();
-            arr.splice(i, 0, res.value);
-
-            arr.push(c);
-        }
+        i++;
+        //if (res.value == " ") {
+        //    let temp = s.substr(inc(s.indexOf(res.value)));
+        //    temp = temp.substr(inc(temp.indexOf(res.value)));
+        //    let i = 0, _c, arr = [];
+        //    c = s[i].toLowerCase();
+        //    res = iter.next();
+        //    while (!res.done) {
+        //        if (res.value < c) {
+        //            arr.unshift.apply(arr, [res.value, c]);
+        //        }
+        //        else if (res.value > c) {
+        //            arr.push(res.value);
+        //            arr.unshift(_c);
+        //        }
+        //        _c = temp[i++];
+        //        res = iter.next();
+        //    }
+        //}
     }
     return sort();
 }
