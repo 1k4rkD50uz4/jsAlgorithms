1606cfb4aaa2d0fb37e757479adffdaf28300563
Mark Dsouza <mark.francis.dsouza@gmail.com>
2022-07-01 01:51:59 -0700

    seq ops after closure

--- binarySort.js
+++ binarySort.js
@@ -1,10 +1,11 @@
 ﻿function main() {
-    const inc = i => ++i;
+    const inc = i => ++i,
+        dec = i => --i;
     let s = "The quick brown fox jumped over the lazy dog",
         iter = s[Symbol.iterator](),
         res = iter.next(),
         c = res.value,
-        arr=[],
+        arr = [],
         prepend = (function () {
             const compare = val => c < val ? -1 : c > val ? 1 : 0;
             let c = res.value;
@@ -28,14 +29,54 @@
         c = res.value;
         if (res.value == " ") {
             let i = inc(s.lastIndexOf(res.value));
-            res.value = s[i];
+            arr.unshift.apply(arr, [String.fromCharCode(parseInt('' + i, 16)).toLowerCase(), s[i]]); 
+            i = inc(inc(i));
+            c = arr.pop();
+            arr.push(s[i]);
+            arr.push(c);
+            res = iter.next(); 
+            c = res.value;
+            res = iter.next();
+            i = 0;
+            c += String.fromCharCode(s[i].toLowerCase().charCodeAt(i));
+            c += res.value;
+            res = iter.next();
+            arr.push(res.value);
+            res = iter.next();
+            arr.splice(inc(i), i, res.value);
+            res = iter.next();
+            arr.push(res.value);
+            res = iter.next();
+            res = iter.next();
+            arr.splice(inc(i), i, res.value);
+            res = iter.next();
+            c = c.split('');
+            c.splice(inc(i), i, res.value);
+            res = iter.next();
+            c.unshift(res.value);
+            res = iter.next();
+            i = arr.length - c.length;
+            c.push(res.value);
+            res = iter.next();
+            c.unshift(res.value);
+            arr = arr.concat(c);
+            res = iter.next();
             i = inc(i);
-            c = s[i]; 
-            i = s.indexOf(c);
-            c = s[inc(i)];
-            while (i != -1) {
-                
-            }
+            res = iter.next();
+            arr.splice(i, 0, res.value);
+            i++;
+            res = iter.next();
+            i++;
+            c = res.value;
+            res = iter.next();
+            c += res.value;
+            i++;
+            res = iter.next();
+            i++;
+            res = iter.next();
+            arr.splice(i, 0, res.value);
+
+            arr.push(c);
         }
     }
     return sort();
