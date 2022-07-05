ef9e854a33e67d4c15b62b815a2654c97193524c
Mark Dsouza <mark.francis.dsouza@gmail.com>
2022-07-05 12:02:11 -0700

    modifying vars with funcs

--- binarySort.js
+++ binarySort.js
@@ -1,28 +1,51 @@
 ﻿function main() {
     const inc = i => ++i,
-        compare = val => c < val ? -1 : c > val ? 1 : 0;
+        compare = (c,val) => c < val ? true : false;
     let s = "The quick brown fox jumped over the lazy dog",
         i = 0,
-        arr = [],
-        cb = inc;
-        prepend = (function () {            
-            let i = 0,
-                j = inc(i),
-                k = inc(j),
-                c = s[k];
-            let f = function () {
-                arr.unshift(c);
-                //c = String.fromCharCode(s[i].toLowerCase().charCodeAt(i));
-                c=s[inc(k)];
-            };
-            return f;
-        })(),
-        done=false;
-    function sort() { 
-        while (!done) {
-            prepend();          
+        c,
+        arr = [];
+        obj =
+        {
+            i: i,
+            cb: function() {
+                this.i++;
+                return s[this.i];
+            }
+        };
+    function sort() {
+        let i = !this.i ? 0 : this.i,
+            c = s[i],
+            res = { value: this.cb(), done: false };
+        if (!set(res.value)) {
+            res.value = this.cb();
         }
-    }
-    return sort();
+        function set(value) {
+            if (compare(c, value)) {
+                if (c == " ") {
+                    let _s = s.slice(++i);
+                    i = _s.indexOf(c);
+                }
+                else {
+                    c = value;
+                }
+            }
+            else {
+                if (c == " ") {
+                    let _s = s.slice(++i);
+                    i = _s.indexOf(c);
+                }
+                else {
+                    arr.push(value);
+                    return false;
+                }
+            }
+        }
+        function get() {
+            
+        }
+        return sort.call(obj)
+    }    
+    return sort.call(obj);
 }
 main();
